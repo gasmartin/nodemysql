9341ee5d92c19d105b89184cdadad68c8c30e953
Gabriel San Martin <gssm.snf19@uea.edu.br>
2020-05-16 20:25:10 -0400

    First commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const port = 3000;
+const hostname = 'localhost';
+
+const server = http.createServer((req, res) => {
+    const method = req.method;
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}/`);
+});
